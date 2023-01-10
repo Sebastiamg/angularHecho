0d5a3996a77f75b7964963d6ceb38849be9a8576
Sebastiamg <sebasortiz112004@gmail.com>
2023-01-10 18:45:38 -0500

    clase0110

---
+++ Angular-Nuevo/src/app/pages/product/product.component.ts
@@ -0,0 +1,31 @@
+//Trabajar con los metodos http de la libreria http client
+import { Component, OnInit } from '@angular/core';
+import { HttpClient as HttpClient } from '@angular/common/http';
+
+// get 
+// post 
+// put
+// delete
+// 😳😳😳😳😳😳😳😳😳😳😳😳😳😳😳😳
+
+@Component({
+  selector: 'app-product',
+  templateUrl: './product.component.html',
+  styleUrls: ['./product.component.css']
+})
+export class ProductComponent {
+  constructor( private httpClient: HttpClient ) {
+  };
+
+  getProducts(): void {
+    const response = this.httpClient.get("https://api.escuelajs.co/api/v1/products").subscribe(response => {
+      console.log(response);
+    });
+    console.log(response);
+  }
+
+  ngOnInit(): void {
+    this.getProducts();
+  };
+};
+
